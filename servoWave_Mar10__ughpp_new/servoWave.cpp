e49b41d255278a0b7170fa5bc5d4eac2bc8d3a9d
Gabriella Levine <gabriella.levine@gmail.com>
2012-03-21 21:59:59 -0400

    new ay

---
+++ servoWave_Mar10__ughpp_new/servoWave.cpp
@@ -0,0 +1,109 @@
+#include "servoWave.h"
+#include "Metro.h"
+ServoWave::ServoWave() {
+}
+ServoWave::~ServoWave() {
+  delete servoPositions;
+  delete servos;
+}
+
+ServoWave::ServoWave(int _numServos, float _angle, float _period, int _wavelength, int _ampMax, int _indexSpacing){
+
+  numServos = _numServos;//servos of the whole unit
+  angle = _angle;//height of wave 
+  period = _period; //in milliseconds
+  wavelength = _wavelength;//how many servos long the wave will be
+  ampMax=_ampMax;
+  indexSpacing=_indexSpacing;
+   a=0;
+  numServos = _numServos;
+  currentServo = 0;//set currentServo to 0
+  long previousMillis = 0;
+  long lastUpdatedAt = millis();
+
+  TOTAL_INDICES=TWO_PI/0.1;//total num of index pioints, until the amplitude is back at zero
+  TOTAL_NODES=10;
+  
+  //initialize servoPosition array
+  for(int i=0;i<numServos;i++){
+    servoPositions[i] = 90; // starting position for all servos. could be passed as an argument     
+  }
+
+  for (int i = 0; i < TOTAL_INDICES; i++) {
+    // wave[i] = (sin(a) + 1.0) * ampMax;//ampMax defines the amount it strays from the 0 position 
+    wave[i] = lmap(sin(a),-1.0,1.0,0.0,ampMax);//180.0);
+    a+=0.1;//
+   // Serial.println(wave[i]);
+  }
+
+  for (int i = 0; i < TOTAL_NODES; i++) {
+    index[i] = i*indexSpacing%TOTAL_INDICES;//i*wavelength;
+  }
+  TOTAL_INDICES=TWO_PI/period;
+  
+  
+}
+  
+
+void ServoWave::update(){
+       // Serial.print(ampMax);
+//  for (int i=0;i<TOTAL_NODES;i++) {
+//    //index[i] = i*indexSpacing;//i*wavelength;
+//    //Serial.print(float(ampMax)/2*(float)wave[index[i]]);
+//        Serial.print((ampMax*wave[index[i]])); 
+//    //Just fyi, multiplying ampMax by  ((float)wave[index[i]]) is changing the max amplitude
+//    Serial.print(" ");
+//    index[i] = (index[i]+1) % TOTAL_INDICES;//TOTAL_INDICES;//numservos in wavelength  
+//  }
+
+a +=0.02;
+float x = a;
+for (int i=0;i<TOTAL_NODES;i++){
+ wave[i] = lmap(sin(x*period),-1.0,1.0,0.0,angle);
+     //wave[i] = lmap(wave[i],-1.0,1.0,0.0,ampMax);//180.0);
+ Serial.print(wave[i]);
+ Serial.print(" ");
+x+=TOTAL_INDICES; 
+}
+  Serial.println();
+}
+
+void ServoWave::calcIndex(float p){
+   for (int i = 0; i < TOTAL_NODES; i++) {
+     indexSpacing = p;
+    index[i] = (index[i]+(i*indexSpacing))%TOTAL_INDICES;//i*wavelength;
+  }
+}
+void ServoWave::calcPeriod(float per){
+//    TOTAL_INDICES=TWO_PI/per;//total num of index pioints, until the amplitude is back at zero
+//     for (int i = 0; i < TOTAL_INDICES; i++) {
+//    // wave[i] = (sin(a) + 1.0) * ampMax;//ampMax defines the amount it strays from the 0 position 
+//    wave[i] = lmap(sin(a),-1.0,1.0,0.0,angle);//180.0);
+//    a+=per;//
+//   
+//  }
+}
+
+float ServoWave::lmap(float val, float inMin, float inMax, float outMin, float outMax) {
+  return outMin + (outMax - outMin) * ((val - inMin) / (inMax - inMin));
+}
+
+void ServoWave::addServo(Servo servo){
+  servos[currentServo]=servo;
+  currentServo++; 
+}
+
+void ServoWave::addServo(Servo servo, int startingPosition){
+  servos[currentServo]=servo;
+  servoPositions[currentServo] = startingPosition;
+  currentServo++; 
+}
+
+void ServoWave::move(){
+  for (int i=0;i<10;i++){
+   // servos[i].write(ampMax/2*wave[index[i]]); 
+   servos[i].write(wave[i]);
+    //changing ampMax will change max amplitude
+  }
+}
+
---
+++ servoWave_Mar10__ughpp_new/servoWave_Mar10__ughpp_new.ino
@@ -0,0 +1,54 @@
+#include <Servo.h>
+
+#include "ServoWave.h"
+#include <Metro.h>
+
+const int analogPin1 = A0 ;
+const int analogPin2 =A1;
+const int analogPin3 = A2;
+
+int lastPeriodState = 0;
+int lastIndexState = 0;
+
+ServoWave servoWave;
+int numServos = 12;//this has to be 15
+
+void setup(){
+  Serial.begin(57600);
+
+  servoWave = ServoWave(numServos, 135, 3, 5, 180, 4);
+  //ServoWave::ServoWave(int _numServos, float _angle, float _period, int _wavelength, int _ampMax, int _indexSpacing){
+  for(int i=0;i<numServos;i++){
+    servoWave.servos[i].attach(i+2);
+    servoWave.addServo(servoWave.servos[i]);
+  }
+}
+
+void loop(){
+  servoWave.ampMax  = map(analogRead(analogPin1), 0,1023, 1,3);
+ servoWave.period = analogRead(analogPin3)*0.0005;// map(analogRead(analogPin2),0,1023,0,5);//check these values...
+  servoWave.indexSpacing  = map(analogRead(analogPin2),0,1023, 1,20);
+
+  for(int i=0;i<12;i++){
+    servoWave.update(); 
+    servoWave.move();
+    
+    if(servoWave.indexSpacing!=lastIndexState){
+      //period = perdiodState;
+     servoWave.calcIndex(servoWave.indexSpacing);
+    //Serial.println(servoWave.indexSpacing); 
+    }
+     lastIndexState=servoWave.indexSpacing;
+     
+     if(servoWave.period !=lastPeriodState){
+  servoWave.calcPeriod(servoWave.period);
+ // Serial.println(servoWave.period);
+}
+lastPeriodState = servoWave.period;
+
+    // Serial.println(periodState);
+  }
+  delay(10);
+}
+
+
